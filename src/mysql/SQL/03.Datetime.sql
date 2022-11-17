7a05852a0ea0bc1798aeaab4d1611cefc9426180
rnaqpddl123@gmail.com <rnaqpddl123@gmail.com>
2022-11-17 11:49:45 +0900

    Reference update

--- src/mysql/SQL/03.Datetime.sql
+++ src/mysql/SQL/03.Datetime.sql
@@ -15,7 +15,7 @@ CREATE TABLE datetable (
 ) AUTO_INCREMENT=101;                       # id 시작번호
 
 /*
- *
+ * 2. 데이터 입력
  */
  # date에 2022-11-17 입력, 나머지 자동생성
 INSERT INTO datetable VALUES (DEFAULT, '2022-11-17', DEFAULT);
@@ -25,6 +25,10 @@ SELECT * FROM datatable;
 INSERT INTO datetable(`date`, `datetime`) VALUES 
 	('2001-11-23', '2001-11-23 11:20:01'),
 	('2005-10-24', '2005-10-24 10:04:35'); 
+
+/*
+ * 3.데이트 조회
+ */
 
 # 2022-11-17 형식의 값을 얻고 싶을 경우
 SELECT `date`, DATE_FORMAT(`datetime`, '%Y-%m-%d') FROM datetable;
@@ -40,3 +44,15 @@ SELECT DATE_FORMAT(`datetime`,'%y-%m-%d %H-%i') AS modifytime
 
 # 날짜 조회 함수
 SELECT NOW(), CURDATE(), CURTIME();
+
+# 날짜 더하기 뺴기 (오늘기준 40일 이후)
+SELECT DATE_ADD(NOW(), INTERVAL 40 DAY);
+ 
+# 날짜 더하기 뺴기 (오늘기준 3달전 )
+SELECT DATE_SUB(NOW(), INTERVAL 3 MONTH);
+`
+# D-day  계산
+SELECT TO_DAYS('2023-03-08') - TO_DAYS(NOW());
+
+# 요일: 1-일요일, 2-월요일....
+SELECT DAYOFWEEK(`datetime`) FROM datetable;
